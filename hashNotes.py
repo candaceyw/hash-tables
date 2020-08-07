a7fac5b13b84d89d83870ef62a6df1826067cd9a
candacewilson <candaceyw@gmail.com>
2020-08-06 21:15:44 -0400

    day 2 notes complete

--- hashNotes.py
+++ hashNotes.py
@@ -18,9 +18,11 @@ class HashTableEntry:
     def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.next= None
+        self.next = None
+
 
 hash_table = [None] * 8
+
 
 def my_hash(s):
     # take every character in the string, and convert character to number
@@ -45,11 +47,11 @@ def put(key, val):
     # find the start of the linked list using the index
     # search through linked list
     # If the key already exists in teh linked list
-        # replace the value
+    # replace the value
     # else
-        # add new HashTable entry to the haed of linked list
+    # add new HashTable entry to the haed of linked list
 
-    ## this can be deleted
+    # this can be deleted
     # CHECK IF SOMETHING ALREADY EXISTS AT THAT INDEX
     # if hash_table[i] != None:
     #     print(f"Collision! Overwriting {repr(hash_table[i])}!")
@@ -68,12 +70,27 @@ def get(key):
     # return the value from the array at the index
     # return hash_table[i]
 
+
 def delete(key):
     # has the key and get an index
     i = hash_index(key)
     # Search through the linked list for the matching key
     # delete that node
     # return value of deleted node (or None)
+
+
+def resize():
+    # Make a new array that's DOUBLE the current size
+    # Go through each linked list in the array
+    # Go through each item and re-has it
+    # insert the items into their new location
+    pass
+
+
+def shrink():
+    # same as resize, but reduce array by HALF
+    pass
+
 
 put("Hello", "Hello Value")
 put("world", "world value")
@@ -82,4 +99,3 @@ print(hash_table)
 # print(my_hash('Hello'))
 # print(my_hash('Hello'))
 # print(my_hash('World'))
-
